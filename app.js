10c9aad661f567b5ada55a795bdf4f9be84edc65
Aad Versteden <madnificent@gmail.com>
2018-02-13 10:57:43 +0100

    Initial import of mu-javascript-template
    Creating a new repository based on the contents of the
    mu-javascirpt-template.  We will contain all code in this repository
    and split off reusable bits later on in the process.  The goal is to
    ensure the code is structured in a logical manner already.

---
+++ app.js
@@ -0,0 +1,24 @@
+import { app, query } from 'mu';
+
+app.get('/', function( req, res ) {
+  res.send({ msg: 'Hello mu-javascript-template' });
+} );
+
+
+app.get('/query', function( req, res ) {
+  var myQuery = `
+    SELECT *
+    WHERE {
+      GRAPH <http://mu.semte.ch/application> {
+        ?s ?p ?o.
+      }
+    }`;
+
+  query( myQuery )
+    .then( function(response) {
+      res.send( JSON.stringify( response ) );
+    })
+    .catch( function(err) {
+      res.send( "Oops something went wrong: " + JSON.stringify( err ) );
+    });
+} );
