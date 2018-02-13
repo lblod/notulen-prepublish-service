d4583a700fa5ef299c93a729087231ccdf2e79d1
Aad Versteden <madnificent@gmail.com>
2018-02-13 14:38:48 +0100

    Use node-walker in example-code
    The app.js file contains example calls allowing for manual testing.
    It now includes an example call which converts the nodes to rich
    nodes when walking over them.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import { app, query } from 'mu';
 import FIXTURES from './fixtures/default';
+import { walk } from './marawa/node-walker';
+import { cleanRichNodes } from './support/rich-node-printer';
+import jsdom from 'jsdom';
 
 app.get('/', function( req, res ) {
   res.send({ msg: 'Hello mu-javascript-template' });
@@ -25,3 +28,21 @@ app.get('/query', function( req, res ) {
 
 app.get('/fixtures/aanstelling', (req, res) => res.send( { content: FIXTURES.aanstelling } ));
 app.get('/fixtures/langeAanstelling', (req, res) => res.send( { content: FIXTURES.langeAanstelling } ));
+
+app.get('/walk/aanstelling', (req, res) => {
+  const dom = new jsdom.JSDOM( FIXTURES.aanstelling );
+  const node = dom.window.document.querySelector("div.annotation-snippet");
+  const walkedNodes = walk( node );
+  const cleanNodes = cleanRichNodes( walkedNodes );
+
+  res.send({ nodes: cleanNodes });
+});
+
+app.get('/walk/langeAanstelling', (req, res) => {
+  const dom = new jsdom.JSDOM( FIXTURES.langeAanstelling );
+  const node = dom.window.document.querySelector("#ember279");
+  const walkedNodes = walk( node );
+  const cleanNodes = cleanRichNodes( walkedNodes );
+
+  res.send({ nodes: cleanNodes });
+});
