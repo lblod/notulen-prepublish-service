532de98f82576553897c00a456f3511b6bf747cb
Aad Versteden <madnificent@gmail.com>
2018-02-13 16:23:59 +0100

    Allow printing of context array
    The context array has a similar problem as the RichNode.  It contains
    content which is recursive.  This implementation of cleaning ensures
    the recursive content is removed from the items, so we can return them
    in the JSON string for demo purposes.

--- support/rich-node-printer.js
+++ support/rich-node-printer.js
@@ -1,6 +1,22 @@
 import {get, set} from '../marawa/ember-object-mock';
 
 /**
+ * Ugly tooling which makes it slightly easier to print RichNodes as
+ * exposed by MARAWA and the likes.  This tooling simply removes
+ * certain properties from the rendered output.
+ */
+function cleanContexts( [ node, ...nodes ] ){
+  node.parent = undefined;
+  node.domNode = undefined;
+  node.richNode = undefined;
+
+  if( nodes.length )
+    return [node, ...cleanContexts(nodes)];
+  else
+    return [node];
+}
+
+/**
  * Walks through the supplied RichNode items, and cleans them from
  *  their recursive content.
  *
@@ -20,4 +36,4 @@ function cleanRichNodes( node ){
   return node;
 }
 
-export { cleanRichNodes };
+export { cleanRichNodes, cleanContexts };
