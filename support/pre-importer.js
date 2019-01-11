33e04f065627062a0e66eb8953c24553ad459fe8
Aad Versteden <madnificent@gmail.com>
2019-01-11 08:33:57 +0100

    Allow signing of agenda
    We can now sign a specific agenda.  This is the intended flow before
    publishing the agenda.  The publication of the agenda itself could
    share quite some code with this flow, but is still to be tackled.

--- support/pre-importer.js
+++ support/pre-importer.js
@@ -1,8 +1,14 @@
+import mu from 'mu';
+
 import {
-  query, update, sparqlEscapeUri, sparqlEscapeString, uuid
+  query, update, sparqlEscapeUri, sparqlEscapeString, sparqlEscapeDateTime, uuid
 } from  'mu';
 
 import { findAllNodesOfType } from './dom-helpers';
+
+function hackedSparqlEscapeString( string ) {
+  return '"""' + string.replace(/[\\"']/g, function(match) { return '\\' + match; }) + '"""';
+};
 
 /**
  * Extracts the Agenda's content from the supplied document.
@@ -26,25 +32,69 @@ async function preImportAgendaFromDoc( doc ) {
   const agendaUri = `http://lblod.info/prepublished-agendas/${agendaUuid}`;
 
   // Create the new prepublished agenda, and dump it in to the store
-  const updatePromise = update( `
+  await update( `
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX pav: <http://purl.org/pav/>
+    PREFIX prov: <http://www.w3.org/ns/prov#>
 
     INSERT {
       ${sparqlEscapeUri(agendaUri)}
-         a ext:PrePublishedAgenda;
-         ext:content ${sparqlEscapeString( agendaContent )};
-         ext:editorDocumentContext ?context;
-         mu:uuid ${sparqlEscapeString( agendaUuid )}.
-      ?documentContainer ext:hasPrepublishedAgenda ${sparqlEscapeUri(agendaUri)}
+         a ext:VersionedAgenda;
+         ext:content ${hackedSparqlEscapeString( agendaContent )};
+         prov:wasDerivedFrom ${sparqlEscapeUri(doc.uri)};
+         mu:uuid ${hackedSparqlEscapeString( agendaUuid )}.
+      ?documentContainer ext:hasVersionedAgenda ${sparqlEscapeUri(agendaUri)}.
     } WHERE {
       ${sparqlEscapeUri(doc.uri)} ^pav:hasVersion ?documentContainer;
                                   ext:editorDocumentContext ?context.
     }`);
 
-  const response = await updatePromise;
-  return response; // should we return this?
+  return agendaUri;
 };
 
-export { extractAgendaContentFromDoc, preImportAgendaFromDoc };
+async function signVersionedAgenda( versionedAgendaUri, sessionId, targetStatus ) {
+  const signedResourceUuid = uuid();
+  const signedResourceUri = `http://lblod.info/signed-resources/${signedResourceUuid}`;
+
+  // TODO: get correct signatorySecret from ACMIDM
+
+  const query = `
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core>
+    PREFIX pav: <http://purl.org/pav/>
+    PREFIX sign: <http://mu.semte.ch/vocabularies/ext/signing/>
+    PREFIX publicationStatus: <http://mu.semte.ch/vocabularies/ext/signing/publication-status/>
+    PREFIX muSession: <http://mu.semte.ch/vocabularies/session/>
+    PREFIX dct: <http://purl.org/dc/terms/>
+
+    DELETE {
+      ${sparqlEscapeUri(versionedAgendaUri)}
+        ext:stateString ?state.
+    } INSERT {
+      ${sparqlEscapeUri(signedResourceUri)}
+        a sign:SignedResource;
+        sign:text ?content;
+        sign:signatory ?userUri;
+        sign:signatoryRoles ?signatoryRole;
+        dct:created ${sparqlEscapeDateTime(new Date())};
+        sign:signatorySecret ?signatorySecret;
+        sign:status publicationStatus:unpublished;
+        dct:subject ${sparqlEscapeUri(versionedAgendaUri)}.
+      ${sparqlEscapeUri(versionedAgendaUri)}
+        ext:stateString ${hackedSparqlEscapeString(targetStatus)}.
+    } WHERE {
+      ${sparqlEscapeUri(versionedAgendaUri)}
+        ext:content ?content.
+      ${sparqlEscapeUri(sessionId)}
+        muSession:account/^foaf:account ?userUri.
+      ${sparqlEscapeUri(sessionId)}
+        ext:sessionRole ?signatoryRole.
+      BIND ("helloworldsecretbehere" AS ?signatorySecret)
+    }`;
+
+  const updatePromise = await update( query );
+  return updatePromise;
+};
+
+export { extractAgendaContentFromDoc, signVersionedAgenda, preImportAgendaFromDoc };
