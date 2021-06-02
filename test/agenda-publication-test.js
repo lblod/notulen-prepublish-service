f63a430e9d4eae6db5cc757a438f7c78ddadd238
Niels Vandekeybus <progster@gmail.com>
2021-06-02 11:00:12 +0200

    use before hook to set up dataset

--- test/agenda-publication-test.js
+++ test/agenda-publication-test.js
@@ -1,4 +1,5 @@
 import  { strict  as assert } from 'assert';
+import { before } from 'mocha';
 import { constructHtmlForAgendaFromData } from '../support/agenda-utils';
 import Meeting from '../models/meeting';
 import AgendaPoint from '../models/agendapoint';
@@ -39,62 +40,55 @@ function constructAgenda() {
 
 const RDF_TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 describe('agenda publication template', function () {
+  before(async function() { this.dataset = await htmlToRdf(constructAgenda());});
+
   it('has the expected administrative body linked to the meeting', async function() {
-    const html = constructHtmlForAgendaFromData(meeting, []);
-    const dataset = await htmlToRdf(html);
     const adminBodyQuad = factory.quad(
       factory.namedNode(meeting.uri),
       factory.namedNode("http://data.vlaanderen.be/ns/besluit#isGehoudenDoor"),
       factory.namedNode(meeting.adminBodyUri)
     );
-    assert(dataset.has(adminBodyQuad));
+    assert(this.dataset.has(adminBodyQuad));
   });
   it('has the expected zitting type', async function() {
-    const html = constructHtmlForAgendaFromData(meeting, []);
-    const dataset = await htmlToRdf(html);
     const typeQuad = factory.quad(
       factory.namedNode(meeting.uri),
       factory.namedNode(RDF_TYPE),
       factory.namedNode("http://data.vlaanderen.be/ns/besluit#Zitting")
     );
-    assert(dataset.has(typeQuad));
+    assert(this.dataset.has(typeQuad));
   });
   it('has the correct planned start date', async function() {
-    const html = constructHtmlForAgendaFromData(meeting, []);
-    const dataset = await htmlToRdf(html);
     const plannedStartQuad = factory.quad(
       factory.namedNode(meeting.uri),
       factory.namedNode("http://data.vlaanderen.be/ns/besluit#geplandeStart"),
       factory.literal(meeting.plannedStart, "http://www.w3.org/2001/XMLSchema#dateTime")
     );
-    assert(dataset.has(plannedStartQuad), "expected planned start");
+    assert(this.dataset.has(plannedStartQuad), "expected planned start");
   });
 
   it('has agendapoint 2 correctly linking to agendapoint1', async function() {
-    const dataset = await htmlToRdf(constructAgenda());
     const addedAfterQuad = factory.quad(
       factory.namedNode(agendapoint2.uri),
       factory.namedNode("http://data.vlaanderen.be/ns/besluit#aangebrachtNa"),
       factory.namedNode(agendapoint1.uri)
     );
-    assert(dataset.has(addedAfterQuad));
+    assert(this.dataset.has(addedAfterQuad));
   });
 
   it('has the correct title for agendapoint 1', async function () {
-    const dataset = await htmlToRdf(constructAgenda());
     const titleQuad = factory.quad(
       factory.namedNode(agendapoint1.uri),
       factory.namedNode("http://purl.org/dc/terms/title"),
       factory.literal(agendapoint1.title)
     );
-    assert(dataset.has(titleQuad));
+    assert(this.dataset.has(titleQuad));
   });
 
   it('validates the basic shacl profile', async function(){
     const shacl = await loadDataset(__dirname + "/shapes/basic-agenda.ttl");
     const validator = new SHACLValidator(shacl, { factory });
-    const triples = await htmlToRdf(constructAgenda());
-    const report = await validator.validate(triples);
+    const report = await validator.validate(this.dataset);
     assert(report.conforms, shaclReportToMessage(report));
   });
 });
