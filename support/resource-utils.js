1dc3efa65c9f2e6ede3ff81344cca8aa3165ccda
Elena Poelman <elena.poelman@redpencil.io>
2022-11-16 12:02:24 +0100

    add utility functions which get the corresponding uri/uuid for a given uuid/uri

---
+++ support/resource-utils.js
@@ -0,0 +1,25 @@
+// @ts-ignore
+import { query, sparqlEscapeString, sparqlEscapeUri } from 'mu';
+
+
+export async function getUri(uuid){
+  const queryResult = await query(
+    `PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+     SELECT * WHERE {
+       ?uri <http://mu.semte.ch/vocabularies/core/uuid> ${sparqlEscapeString( uuid )}
+     }`);
+  if (!queryResult.results.bindings.length)
+    throw new Error('URI not found for supplied uuid' + uuid);
+  return queryResult.results.bindings[0].uri;
+}
+
+export async function getUuid(uri){
+  const queryResult = await query(
+    `PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+     SELECT * WHERE {
+       ${sparqlEscapeUri(uri)} <http://mu.semte.ch/vocabularies/core/uuid> ?uuid
+     }`);
+  if (!queryResult.results.bindings.length)
+    throw new Error('URI not found for supplied URI' + uri);
+  return queryResult.results.bindings[0].uuid;
+}
