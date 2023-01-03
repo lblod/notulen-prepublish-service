c1ae3d72572435e40338b2bda1ee3de8d0539181
Oscar <lagartoverde97@gmail.com>
2023-01-03 10:36:20 +0100

    check for length

--- support/besluit-exporter.js
+++ support/besluit-exporter.js
@@ -23,7 +23,7 @@ async function buildBesluitenLijstForMeeting(meeting, meetingUuid) {
   const treatmentsWithDecisions = treatments.filter((t) => t.decisions.length > 0);
   const html = constructHtmlForDecisionList(meeting, treatmentsWithDecisions);
   const errors = meeting.validate();
-  if(!treatmentsWithDecisions) {
+  if(!treatmentsWithDecisions.length) {
     return {html, errors: [...errors, 'No besluits found']};
   }
   return {html, errors};
